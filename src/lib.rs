062edf6b4067cc9ed7758de1a9b7881068a8fd53
watcol <potfman@gmail.com>
2021-04-04 17:39:20 +0900

    Add basic implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,27 @@
 //! clap_generate implementation for markdown.
+extern crate clap;
+extern crate clap_generate;
+
+use clap::App;
+use clap_generate::Generator;
+use std::io::Write;
+
+// A macro to automatically unwraps `write!`.
+macro_rules! w {
+    ($($arg:tt)*) => {
+        write!($($arg)*).expect("Failed to write to generated file")
+    }
+}
+
+/// `clap_generate::Generator` implementer for Markdown.
+pub struct Markdown;
+
+impl Generator for Markdown {
+    fn file_name(name: &str) -> String {
+        format!("{}.md", name)
+    }
+
+    fn generate(app: &App, buf: &mut dyn Write) {
+        w!(buf, "{}", app);
+    }
+}
