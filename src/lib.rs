d709f6f65cebe2b29cb222848afc8a29d6baa2e2
watcol <potfman@gmail.com>
2021-04-04 18:01:03 +0900

    Add conventional functions.

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,32 @@ impl Generator for Markdown {
         w!(buf, "{}", app);
     }
 }
+
+/// Write markdown to specified buffer. (Wraps `clap_generate::generate`.)
+pub fn write_markdown<S: Into<String>>(app: &mut App, bin_name: S, buf: &mut dyn Write) {
+    clap_generate::generate::<Markdown, _>(app, bin_name, buf);
+}
+
+/// Write markdown to `String`. (Wraps `write_markdown`.)
+pub fn to_markdown<S: Into<String>>(
+    app: &mut App,
+    bin_name: S,
+) -> Result<String, std::string::FromUtf8Error> {
+    let mut buf = Vec::new();
+    write_markdown(app, bin_name, &mut buf);
+    String::from_utf8(buf)
+}
+
+/// Write markdown to specified dir. (Wraps `clap_generate::generate_to`.)
+pub fn write_markdown_to<S: Into<String>, T: Into<std::ffi::OsString>>(
+    app: &mut App,
+    bin_name: S,
+    out_dir: T,
+) {
+    clap_generate::generate_to::<Markdown, _, _>(app, bin_name, out_dir);
+}
+
+/// Print markdown to stdout. (Wraps `write_markdown`.)
+pub fn print_markdown<S: Into<String>>(app: &mut App, bin_name: S) {
+    write_markdown(app, bin_name, &mut std::io::stdout());
+}
